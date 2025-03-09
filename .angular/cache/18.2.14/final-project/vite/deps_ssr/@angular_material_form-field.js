c0c2ec64b56188263b8fce794bc4a0e59fb40d9d
Avital2025” <A0583273344@gmail.com>
2025-03-09 20:07:36 +0200

    move project files to root directory

---
+++ .angular/cache/18.2.14/final-project/vite/deps_ssr/@angular_material_form-field.js
@@ -0,0 +1,45 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  MAT_ERROR,
+  MAT_FORM_FIELD,
+  MAT_FORM_FIELD_DEFAULT_OPTIONS,
+  MAT_PREFIX,
+  MAT_SUFFIX,
+  MatError,
+  MatFormField,
+  MatFormFieldControl,
+  MatFormFieldModule,
+  MatHint,
+  MatLabel,
+  MatPrefix,
+  MatSuffix,
+  getMatFormFieldDuplicatedHintError,
+  getMatFormFieldMissingControlError,
+  getMatFormFieldPlaceholderConflictError,
+  matFormFieldAnimations
+} from "./chunk-HG4BGDUD.js";
+import "./chunk-NQHMTSAL.js";
+import "./chunk-NNIRBLXK.js";
+import "./chunk-O6XA6Z7E.js";
+import "./chunk-BHSDW5A7.js";
+import "./chunk-NQ4HTGF6.js";
+export {
+  MAT_ERROR,
+  MAT_FORM_FIELD,
+  MAT_FORM_FIELD_DEFAULT_OPTIONS,
+  MAT_PREFIX,
+  MAT_SUFFIX,
+  MatError,
+  MatFormField,
+  MatFormFieldControl,
+  MatFormFieldModule,
+  MatHint,
+  MatLabel,
+  MatPrefix,
+  MatSuffix,
+  getMatFormFieldDuplicatedHintError,
+  getMatFormFieldMissingControlError,
+  getMatFormFieldPlaceholderConflictError,
+  matFormFieldAnimations
+};
+//# sourceMappingURL=@angular_material_form-field.js.map
---
+++ dist/final-project/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-EWZST7VE.mjs";import"./chunk-JIZJDUDF.mjs";import"./chunk-LEC2NGT6.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
